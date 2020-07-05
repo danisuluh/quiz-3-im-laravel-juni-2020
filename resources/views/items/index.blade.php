1246dbd57f7832a9bc915b79758f66add9d4cbb6
danisuluh <62416095+danisuluh@users.noreply.github.com>
2020-07-05 13:56:15 +0800

    Update Master Template dan Tempat Gambar ERD
    Update Template dan Route 1

---
+++ resources/views/items/index.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.master')
+
+@section('content')
+  <div class="container">
+    <h1 class="text-center">HASIL TUGAS ERD</h1>
+
+    <img src="{{asset('img/tugas_erd.png')}}" alt="Tugas ERD - Quiz 3" class="img-fluid rounded">
+  </div>
+    
+@endsection
